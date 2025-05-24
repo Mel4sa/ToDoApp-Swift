5d5f5285abdd017d0ca520d303fa9785cabe570b
Mel4sa <melisaxsimsek@gmail.com>
2025-05-24 23:05:31 +0300

    create CategoryController

---
+++ ToDo/Controllers/CategoryViewController.swift
@@ -0,0 +1,63 @@
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    var categories = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadCategories()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let category = categories[indexPath.row]
+        cell.textLabel?.text = category.name ?? "No Name"
+        return cell
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Category", message: nil, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) { _ in
+            let newCategory = Category(context: self.context)
+            newCategory.name = textField.text ?? "Unnamed Category"
+            self.categories.append(newCategory)
+            self.saveCategories()
+    }
+  
+
+        alert.addTextField { field in
+            field.placeholder = "Enter category name"
+            textField = field
+        }
+
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+
+    func saveCategories() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving categories: \(error)")
+        }
+        tableView.reloadData()
+    }
+
+    func loadCategories() {
+        let request: NSFetchRequest<Category> = Category.fetchRequest()
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error fetching categories: \(error)")
+        }
+        tableView.reloadData()
+    }
+}
